bdaa0dc8c734af45162f0a0f88c56efff9a24a12
zarjin <zarjinislamjewel@gmail.com>
2025-04-11 19:40:19 +0600

    add

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -1,15 +1,15 @@
-import { useContext } from "react";
-import { Navigate } from "react-router";
-import { AuthContext } from "../context/AuthContext";
+import { useContext } from 'react'
+import { Navigate } from 'react-router'
+import { AuthContext } from '../context/AuthContext'
 
 const ProtectedRoute = ({ children }) => {
-  const { isLoggedIn } = useContext(AuthContext);
+  const { isLoggedIn } = useContext(AuthContext)
 
   if (!isLoggedIn) {
-    return <Navigate to="/login" replace />;
+    return <Navigate to="/login" replace />
   }
 
-  return children;
-};
+  return children
+}
 
-export default ProtectedRoute;
+export default ProtectedRoute
