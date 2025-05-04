626d5eba774ab3205d62127a89631bb885c0254b
zarjin <zarjinislamjewel@gmail.com>
2025-05-04 14:22:50 +0600

    feat: restore product routes; implement CRUD operations for products with authentication and image upload

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,11 @@
-import express from "express";
-import cookieParser from "cookie-parser";
-import cors from "cors";
-import "dotenv/config";
-import mongodbConnect from "./configs/mongodb.connect.js";
-import authRouter from "./routes/auth.routes.js";
-import userRouter from "./routes/user.routes.js";
-import productRouter from "./routes/product.roures.js";
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import 'dotenv/config';
+import mongodbConnect from './configs/mongodb.connect.js';
+import authRouter from './routes/auth.routes.js';
+import userRouter from './routes/user.routes.js';
+import productRouter from './routes/product.routes.js';
 
 const app = express();
 mongodbConnect();
@@ -20,9 +20,9 @@ app.use(
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/api/auth", authRouter);
-app.use("/api/user", userRouter);
-app.use("/api/product", productRouter);
+app.use('/api/auth', authRouter);
+app.use('/api/user', userRouter);
+app.use('/api/product', productRouter);
 
 const PORT = process.env.PORT;
 
